66f843c53b467659cbd62983bb127bd73708b41f
Michael Oswald <michaelmoswald@gmail.com>
2017-11-30 22:17:04 -0500

    Initial batch of files for project

---
+++ src/main/java/com/flight/bot/flightwebapp/SimpleController.java
@@ -0,0 +1,21 @@
+package com.flight.bot.flightwebapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Michael on 11/30/2017.
+ */
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "index";
+    }
+}
