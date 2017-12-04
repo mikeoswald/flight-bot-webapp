7318daed019c0a51cc76a3f56b94cf751d12ef09
Michael Oswald <michaelmoswald@gmail.com>
2017-12-04 08:01:44 -0500

    updates

--- src/main/java/com/flight/bot/flightwebapp/SimpleController.java
+++ src/main/java/com/flight/bot/flightwebapp/SimpleController.java
@@ -1,5 +1,6 @@
 package com.flight.bot.flightwebapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 /**
  * Created by Michael on 11/30/2017.
@@ -25,12 +27,17 @@ public class SimpleController {
     }
 
     @RequestMapping(value = "/register-bot", method = RequestMethod.POST)
-    public ModelAndView createNewUser(@RequestBody Bot bot) {
-        System.out.println( "BOT NAME YO: "+bot.getName());
+    @ResponseBody
+    public ModelAndView createNewUser(String json) throws IOException {
+        System.out.println( "BOT NAME YO: "+json);
+        ObjectMapper mapper = new ObjectMapper();
+        Bot bot = mapper.readValue("{\"name\": \"John1\"}", Bot.class);
+
+        System.out.println(bot.getName()); //John1post
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("successMessage", "User has been registered successfully");
-        modelAndView.addObject("user", new SecurityProperties.User());
+       // modelAndView.addObject("user", new SecurityProperties.User());
         modelAndView.setViewName("registration");
 
         return modelAndView;
